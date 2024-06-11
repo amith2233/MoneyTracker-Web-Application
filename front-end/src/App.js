5f627466c4cbec2d86d70d59e7d37c563e3c793e
Amith Reddy <amithreddynalla2233@gmail.com>
2024-06-11 17:52:41 -0400

    adding home component and updating app component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route, Routes, Navigate} from "react-router-dom";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import Home from "./components/Home";
+// import LoginToken from "./components/LoginToken";
+import Landing from "./components/Landing";
+import ProtectedRoute from "./components/ProtectedRoute";
+import {useContext} from "react";
+import {MyContext} from "./components/MyContext";
+import AddExpense from "./components/AddExpense";
 
 function App() {
+
+    const {token,setToken}=useContext(MyContext);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            <Routes>
+                <Route exact path="/" element={token ?<Home token={token}/> : <Home/>}/>
+                <Route path="/signup" element={token ? <Navigate to="/landing"/> : <Signup/>} />
+                <Route path="/login" element={ token ? <Navigate to="/landing"/> : <Login setToken={setToken}/>} />
+                <Route path="/landing" element={<ProtectedRoute token={token}><Landing/></ProtectedRoute>}/>
+                <Route path="/addexpense" element={<AddExpense/>}/>
+            </Routes>
+
     </div>
   );
 }
