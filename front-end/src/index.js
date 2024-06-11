dbe09e63783741277bb7667fa460291bde1b5bcd
Amith Reddy <amithreddynalla2233@gmail.com>
2024-06-11 17:43:03 -0400

    Adding broweser router and context provider to index.js file

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import {MyProvider} from "./components/MyContext";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <MyProvider>
+      <BrowserRouter>
+       <App />
+      </BrowserRouter>
+      </MyProvider>
+
   </React.StrictMode>
 );
 
